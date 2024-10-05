30f81d656078039e1920a9f0aa4547b75104de04
MandeepPaul <113959405+MandeepPaul@users.noreply.github.com>
2024-10-04 23:31:03 -0400

    Added auto-domain and auto-tour

---
+++ src/app/setting/app-tour/page.tsx
@@ -0,0 +1,11 @@
+const PageTour = () => {
+  return (
+    <div className=" mt-[10rem] flex flex-col  items-center gap-y-4">
+      <h2 className="text-3xl">App Tour Change</h2>
+      <input className="h-[20rem] w-[30rem] border-2 border-white"></input>
+      <button className="w-20 border-2 border-white">Submit</button>
+    </div>
+  )
+}
+
+export default PageTour
