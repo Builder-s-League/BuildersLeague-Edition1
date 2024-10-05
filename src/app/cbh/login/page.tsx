544e6a7179983cc5db6314abe4f463746fc4ea16
Vinicius Okamoto <leo_kamoto@hotmail.com>
2024-10-05 08:03:04 -0500

    feat: Add HR login page component
    Co-authored-by: Nicole Okamoto <nicoleokamoto@icloud.com>

---
+++ src/app/cbh/login/page.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link'
+
+export default function CBHLoginPage() {
+  return (
+    <div className="flex w-full flex-1 flex-col justify-center gap-2 px-8 sm:max-w-md">
+      <form className="flex w-full flex-1 flex-col justify-center gap-2 text-foreground animate-in">
+        <label className="text-md" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="mb-6 rounded-md border bg-inherit px-4 py-2"
+          name="email"
+          placeholder="you@example.com"
+          required
+        />
+        <label className="text-md" htmlFor="password">
+          Password
+        </label>
+        <input
+          className="mb-6 rounded-md border bg-inherit px-4 py-2"
+          type="password"
+          name="password"
+          placeholder="••••••••"
+          required
+        />
+        <button className="mb-2 rounded-md bg-green-700 px-4 py-2 text-foreground">
+          Sign In
+        </button>
+        <Link href="">
+          <p className="text-sm text-blue-700">Forgot the password?</p>
+        </Link>
+      </form>
+    </div>
+  )
+}
