422ce7d7d746e847f37a4ecd7216c4e82384636d
thejasveer13 <thejasveer13@gmail.com>
2024-10-04 22:23:38 -0500

    Added Resource page,Resource client component, Working on ui

---
+++ src/app/resource/page.tsx
@@ -0,0 +1,16 @@
+import { Resource } from '@/components/Resource'
+import { useState } from 'react'
+
+export default function Page() {
+  // const supabase = createBrowserClient()
+
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const { data } = await supabase.from('notes').select()
+  //     setNotes(data)
+  //   }
+  //   getData()
+  // }, [])
+
+  return <Resource />
+}
---
+++ src/components/Resource.tsx
@@ -0,0 +1,166 @@
+'use client'
+
+import { useState } from 'react'
+
+export const Resource = () => {
+  const [search, setSearch] = useState('')
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch((prev) => e.target.value)
+  }
+
+  return (
+    <div className="flex flex-col items-center gap-10 p-10">
+      <Title title={'this is my title'} />
+      <Input placeholder="search" onChange={handleInput} value={search}></Input>
+      <Description
+        text={
+          "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        }
+      />
+
+      <FileInput />
+      <QuesAnsElement ques={'ssss'} opt={'sesedede'} />
+    </div>
+  )
+}
+const QuesAnsElement = ({ ques, opt }: { ques: string; opt: string }) => {
+  return (
+    <div>
+      <Question ques={ques} />
+      <Option option={opt} />
+    </div>
+  )
+}
+
+const Question = ({ ques }: { ques: string }) => {
+  return <div className="text-sm font-thin">{ques}</div>
+}
+const Option = ({ option }: { option: string }) => {
+  return <div className="text-sm font-thin">{option}</div>
+}
+
+const Card = () => {
+  return <div className="h-32 w-full rounded-md p-2 "></div>
+}
+function Title({ title }: { title: string }) {
+  return (
+    <div className="flex justify-center border-slate-400 text-3xl font-bold">
+      {title}
+    </div>
+  )
+}
+
+const Description = ({ text }: { text: string }) => {
+  return (
+    <p className="mb-3 w-full text-left text-gray-500 dark:text-gray-400">
+      {text}
+    </p>
+  )
+}
+const Input = ({
+  placeholder,
+  onChange,
+  value,
+}: {
+  placeholder: string
+  value: string
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => any
+}) => {
+  return (
+    <div className="w-full max-w-[16rem]">
+      <div className="relative">
+        <input
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChange(e)}
+          id="npm-install-copy-button"
+          type="text"
+          className="col-span-6 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-500 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-400 dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+          value={value}
+        />
+        <button
+          data-copy-to-clipboard-target="npm-install-copy-button"
+          data-tooltip-target="tooltip-copy-npm-install-copy-button"
+          className="absolute end-2 top-1/2 inline-flex -translate-y-1/2 items-center justify-center rounded-lg p-2 text-gray-500 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-800"
+        >
+          <span id="default-icon">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-3.5 w-3.5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z"
+              />
+            </svg>
+          </span>
+          <span id="success-icon" className="inline-flex hidden items-center">
+            <svg
+              className="h-3.5 w-3.5 text-blue-700 dark:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 16 12"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5.917 5.724 10.5 15 1.5"
+              />
+            </svg>
+          </span>
+        </button>
+        <div
+          id="tooltip-copy-npm-install-copy-button"
+          role="tooltip"
+          className="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-gray-700"
+        >
+          <span id="default-tooltip-message">Copy to clipboard</span>
+          <span id="success-tooltip-message" className="hidden">
+            Copied!
+          </span>
+          <div className="tooltip-arrow" data-popper-arrow></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const FileInput = () => {
+  return (
+    <div className="flex w-full items-center justify-center">
+      <label className="flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600 dark:hover:bg-gray-800">
+        <div className="flex flex-col items-center justify-center pb-6 pt-5">
+          <svg
+            className="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 16"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+            />
+          </svg>
+          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+            <span className="font-semibold">Click to upload</span> or drag and
+            drop
+          </p>
+          <p className="text-xs text-gray-500 dark:text-gray-400">
+            SVG, PNG, JPG or GIF (MAX. 800x400px)
+          </p>
+        </div>
+        <input id="dropzone-file" type="file" className="hidden" />
+      </label>
+    </div>
+  )
+}
