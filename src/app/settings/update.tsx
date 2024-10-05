9e7df7dc1cb7db6715beae5388132f6a9d39aca8
rrc_w2024_kVo <kvo@academic.rrc.ca>
2024-10-04 22:43:24 -0500

    Initialized test command.

---
+++ src/app/settings/update.tsx
@@ -0,0 +1,13 @@
+function Update() {
+    return (
+        <div>
+            <button>Organization name:</button>
+            <button>Email:</button>
+            <button>Phone number:</button>
+            <button>Test account info</button>
+            <button>Contact us</button>
+            <button>Update</button>
+            <button>Feedback</button>
+        </div>
+    )
+}
