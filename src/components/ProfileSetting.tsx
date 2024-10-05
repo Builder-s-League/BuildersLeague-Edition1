f07896f9ac52734d0e3db2f8545a8853c74028ad
MandeepPaul <113959405+MandeepPaul@users.noreply.github.com>
2024-10-05 12:25:52 -0400

    Enabled photo change feature

--- src/components/ProfileSetting.tsx
+++ src/components/ProfileSetting.tsx
@@ -1,4 +1,28 @@
+'use client'
+import { useState } from 'react'
+
 export const ProfileSetting = () => {
+  const [fileContent, setFileContent] = useState<null | string>(null)
+  console.log(fileContent)
+
+  const profileChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]
+    if (file) {
+      const fileReader = new FileReader()
+
+      fileReader.onload = (evt: ProgressEvent<FileReader>) => {
+        console.log(evt.target?.result)
+        setFileContent(evt.target?.result as string)
+      }
+
+      fileReader.onerror = (error) => {
+        console.error('Error reading file:', error)
+      }
+
+      fileReader.readAsDataURL(file)
+    }
+  }
+
   return (
     <div className="mx-auto flex w-fit flex-col items-center rounded-2xl border-2 border-white p-6">
       <h1 className="mb-4 text-center text-2xl font-bold">Profile Settings</h1>
@@ -32,28 +56,43 @@ export const ProfileSetting = () => {
             </button>
           </div>
         </div>
-        <div className="m-4 flex flex-col items-center rounded-lg p-4">
+        <div className="group m-4 flex flex-col items-center rounded-lg p-4">
           <div className="relative">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="h-10 w-10"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+            {fileContent ? (
+              <img
+                src={fileContent}
+                alt="Uploaded"
+                className="h-20 w-20 rounded-full object-cover"
               />
-            </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                height={100}
+                width={100}
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-20 rounded-full"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
+              </svg>
+            )}
 
-            <button className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform text-3xl text-gray-500 opacity-0 transition-opacity hover:opacity-100">
-              ✎
-            </button>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={profileChangeHandler}
+              className="size-20 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform cursor-pointer text-3xl text-gray-500 opacity-0"
+            />
           </div>
-          <label className="mt-2 italic text-gray-500">@johndoe</label>
+          <label className="mt-2 italic text-gray-500 group-hover:text-white">
+            Edit Image
+          </label>
         </div>
       </div>
       <div className="border-grey flex gap-2 rounded-lg border-2 p-4">
