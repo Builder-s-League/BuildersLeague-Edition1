2218fb84cdb3e0f4af526c4b1b36d2160e455c42
thejasveer13 <thejasveer13@gmail.com>
2024-10-04 22:59:31 -0500

    minor ui fix

--- src/components/Resource.tsx
+++ src/components/Resource.tsx
@@ -63,7 +63,7 @@ export const Resource = () => {
 }
 const Actions = () => {
   return (
-    <div className="1 justify-betweengit  flex w-full">
+    <div className="1 flex  w-full justify-between">
       <button
         type="button"
         className="mb-2 me-2 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
