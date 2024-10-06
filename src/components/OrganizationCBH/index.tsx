55d6d600116649b24fa29dbe5f255da399c5835f
wangyue <wy950408@gmail.com>
2024-10-06 09:45:57 -0500

    link to edit page

--- src/components/OrganizationCBH/index.tsx
+++ src/components/OrganizationCBH/index.tsx
@@ -4,18 +4,20 @@ import { buttonVariants } from '@/components/ui/button'
 import { OrganizationType } from './type'
 
 type Props = {
-  idx: number
   org: OrganizationType
 }
 
-export default function OrganizationCBH({ idx, org }: Props) {
+export default function OrganizationCBH({ org }: Props) {
   return (
-    <div key={idx} className="mb-4 grid grid-cols-4 gap-4 p-4">
+    <div key={org.id} className="mb-4 grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3 content-center rounded border text-center text-lg">
         {org.name}
       </div>
       <div className="flex flex-col gap-2">
-        <Link href="#" className={buttonVariants({ variant: 'default' })}>
+        <Link
+          href={`/cbh/organization-dashboard/edit/${org.id}`}
+          className={buttonVariants({ variant: 'default' })}
+        >
           Edit Org
         </Link>
         <Link href="#" className={buttonVariants({ variant: 'secondary' })}>
