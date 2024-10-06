25b7fde767af64f7a51212483d5474b4ca834820
Vinicius Okamoto <leo_kamoto@hotmail.com>
2024-10-06 09:27:18 -0500

    style: update login form button spacing

--- src/components/login-form/index.tsx
+++ src/components/login-form/index.tsx
@@ -5,12 +5,12 @@ import { Button } from '@/components/ui/button'
 
 export default function LoginForm() {
   return (
-    <form className="gap-z flex w-full flex-1 flex-col justify-center text-foreground animate-in">
+    <form className="gap-z flex w-full flex-1 flex-col justify-center gap-2 text-foreground animate-in">
       <Label className="text-md" htmlFor="email" />
       <Input name="email" placeholder="you@example.com" required />
       <Label className="text-md" htmlFor="password" />
       <Input name="password" type="password" required placeholder="*******" />
-      <Button className="my-4">Sign In</Button>
+      <Button className="my-2">Sign In</Button>
       <Link href="">
         <p className="text-sm text-blue-700">Forgot the password?</p>
       </Link>
