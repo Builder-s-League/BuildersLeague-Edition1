bf28d414680048d6775f403b89ee60b849f9b7f4
samir-otero <161958707+samir-otero@users.noreply.github.com>
2024-10-06 10:29:20 -0500

    Update hr/settings/page.tsx

--- src/app/hr/settings/page.tsx
+++ src/app/hr/settings/page.tsx
@@ -38,41 +38,36 @@ export default function Settings() {
     }
 
   return (
-    <div className="flex flex-col w-3/4">
-      <div className="flex flex-row w-full">
+    <div className="flex flex-col w-1/2 border border-white">
+      <div className="flex flex-row w-full ">
         <div className="inline-flex w-9/12 gap-6">
           <Button variant="outline" className="w-24">E</Button>
           <Button variant="outline" className="w-24">UGC</Button>
           <Button variant="destructive" className="w-24">Settings</Button>
         </div>
-        <div className="w-1/4 ml-14">
-          <Button className="flex flex-col w-24 py-2/25 px-3/100 gap-3/20 ml-18/100">
+        <div className="w-1/4">
+          <Button className="flex flex-col w-24 py-2/25">
             <Link href="/login">Login/Out</Link>
           </Button>
         </div>
       </div>
-      <div className="flex">
-        <div className="px-8 py-8 w-96">
-          <p className="inline-flex m-1 w-full items-center">Organization name:
+      <div className="flex justify-evenly">
+        <div className="py-8 w-80 flex flex-col items-center">
+          
             <Fragment>{
-              isPTag ? (<span className="ml-2">{organizationNameUpdate}</span>) : (<Input className="ml-1 float-right w-1/2" onChange={e => setOrganizationName(e.target.value)} value={organizationName} type="text"></Input>)}
+              isPTag ? (<p className="inline-flex m-1 w-3/4 items-center">Organization name: {organizationNameUpdate}</p>) : (<Input className="ml-1 float-right w-3/4" placeholder="Organization name" onChange={e => setOrganizationName(e.target.value)} value={organizationName} type="text"></Input>)}
             </Fragment>
-          </p>
-          <p className="inline-flex m-1 w-full items-center">Email:
             <Fragment>{
-              isPTag ? (<span className="ml-2">{emailUpdate}</span>) : (<Input className="ml-1 float-right w-1/2" onChange={e => setEmail(e.target.value)} value={email} type="email"></Input>)}
+              isPTag ? (<p className="inline-flex m-1 w-3/4 items-center">Email: {emailUpdate}</p>) : (<Input className="ml-1 float-right w-3/4" placeholder="Email" onChange={e => setEmail(e.target.value)} value={email} type="email"></Input>)}
             </Fragment>
-          </p>
-          <p className="inline-flex m-1 w-full items-center">Phone number:
             <Fragment>{
-              isPTag ? (<span className="ml-2">{phoneUpdate}</span>) : (<Input className="ml-1 float-right w-1/2" onChange={e => setPhone(e.target.value)} value={phone} type="tel"></Input>)}
+              isPTag ? (<p className="inline-flex m-1 w-3/4 items-center">Phone number: {phoneUpdate}</p>) : (<Input className="ml-1 float-right w-3/4" placeholder="Phone number"  onChange={e => setPhone(e.target.value)} value={phone} type="tel"></Input>)}
             </Fragment>
-          </p>
           <Fragment>{
             isUpdate ? (<Button className="flex flex-col w-44 m-1" variant="outline" onClick={handleCLick}>Update Organization</Button>) :
               (<div className="flex mt-1 gap-4">
-                <Button className="flex flex-col w-1/4" variant="outline" onClick={updateClick}>Update</Button>
-                <Button className="flex flex-col w-1/4" variant="outline" onClick={cancelClick}>Cancel</Button>
+                <Button className="flex flex-col w-1/2" variant="outline" onClick={updateClick}>Update</Button>
+                <Button className="flex flex-col w-1/2" variant="outline" onClick={cancelClick}>Cancel</Button>
               </div>)
           }</Fragment>
         </div>
@@ -81,7 +76,7 @@ export default function Settings() {
             <Button className="flex flex-col w-32 mt-7" variant="secondary">Test account info</Button>
             <Button className="flex flex-col w-32 mt-2" variant="secondary">Contact us</Button>
           </div>
-          <div className="mt-10">
+          <div className="mt-10 mb-8">
             <Button className="flex flex-col w-20" variant="secondary">Feedback</Button>
           </div>
         </div>
