7e6debec66ac7ee93318413ea4e1e0ea560c78c2
Nicole Okamoto <nicoleokamoto@icloud.com>
2024-10-06 09:51:31 -0400

    CBH/myl-login page added #78

---
+++ src/app/cbh/my-login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from '@/components/login-form'
+
+export default function CBHLoginPage() {
+  return (
+    <div className="flex w-full flex-1 flex-col justify-center gap-2 px-8 sm:max-w-md">
+      <LoginForm />
+    </div>
+  )
+}
