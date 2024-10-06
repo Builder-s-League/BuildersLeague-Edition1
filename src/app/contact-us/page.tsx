17a32de0c5dddc5116d0f6bc436fbe350b1ed63d
Anmol Sharma <keepupsharma@gmail.com>
2024-10-06 16:01:41 -0500

    fix build error

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,19 +1,20 @@
-import { Button } from "@/components/ui/button"
-import { Separator } from "@/components/ui/separator"
-
+import { Button } from '@/components/ui/button'
+import { Separator } from '@radix-ui/react-dropdown-menu'
 export default function Contact() {
   return (
-    <div className="flex flex-col items-center m-auto gap-8">
+    <div className="m-auto flex flex-col items-center gap-8">
       <h1 className="text-4xl font-medium leading-none">Contact us</h1>
       <div className="flex flex-col items-center">
-        <p className="text-m text-muted-foreground">Want to renew/cancel subscription?</p>
-        <p className="text-m text-muted-foreground">Here is our contact information:</p>
+        <p className="text-m text-muted-foreground">
+          Want to renew/cancel subscription?
+        </p>
+        <p className="text-m text-muted-foreground">
+          Here is our contact information:
+        </p>
 
-        <Separator className="mt-2" />
-        <div className="flex h-8 items-center space-x-4 text-sm w-full">
-          <div className="w-1/2 h-4 flex justify-end">Email address</div>
-          <Separator orientation="vertical" />
-          <div className="w-1/2 h-4 flex justify-start">Contact Number</div>
+        <div className="flex h-8 w-full items-center space-x-4 text-sm">
+          <div className="flex h-4 w-1/2 justify-end">Email address</div>
+          <div className="flex h-4 w-1/2 justify-start">Contact Number</div>
         </div>
       </div>
 
