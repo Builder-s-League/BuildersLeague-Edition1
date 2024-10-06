72ba0d7b1c70465ffb82576bc1c0fdc56d50147c
Bernard <oyebenard@gmail.com>
2024-10-05 22:28:12 -0500

    Topic Detail Pagr

--- src/app/emp/resources/page.tsx
+++ src/app/emp/resources/page.tsx
@@ -1,5 +1,5 @@
 import { Resource } from '@/components/Resource'
-import { useState } from 'react'
+import { HamburgerMenuIcon } from '@radix-ui/react-icons'
 
 export default function Page() {
   // const supabase = createBrowserClient()
@@ -12,5 +12,18 @@ export default function Page() {
   //   getData()
   // }, [])
 
-  return <Resource />
+  return (
+    <>
+      <nav className="flex h-28 w-full items-center justify-between bg-gray-900 px-6 py-4 text-white">
+        <button className="text-white focus:outline-none">
+          <HamburgerMenuIcon className="h-8 w-8" />
+        </button>
+        <h1 className="text-3xl">Resource Detail</h1>
+        <button className="text-smp rounded-full bg-white px-4 py-2 text-black">
+          Feedback
+        </button>
+      </nav>
+      <Resource />
+    </>
+  )
 }
