8c6a675af8fa675d72df945a8e7bad16de0edccd
thejasveer13 <thejasveer13@gmail.com>
2024-10-05 21:28:28 -0500

    Completed #12

--- src/components/MenuDrawer/index.tsx
+++ src/components/MenuDrawer/index.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react'
-import { Minus, Plus, X as CloseIcon, Menu as MenuIcon } from 'lucide-react'
+import {
+  Minus,
+  Plus,
+  X as CloseIcon,
+  Menu as MenuIcon,
+  ArrowRight,
+} from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
 import {
@@ -12,69 +18,44 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from '@/components/ui/drawer'
+import Link from 'next/link'
+import { mockedTypeLinks } from '@/mocks/type-links'
 
-const data = [
-  {
-    goal: 400,
-  },
-  {
-    goal: 300,
-  },
-  {
-    goal: 200,
-  },
-  {
-    goal: 300,
-  },
-  {
-    goal: 200,
-  },
-  {
-    goal: 278,
-  },
-  {
-    goal: 189,
-  },
-  {
-    goal: 239,
-  },
-  {
-    goal: 300,
-  },
-  {
-    goal: 200,
-  },
-  {
-    goal: 278,
-  },
-  {
-    goal: 189,
-  },
-  {
-    goal: 349,
-  },
-]
-
-export function MenuDrawer() {
+export default function MenuDrawer() {
+  const topics = mockedTypeLinks
   return (
     <Drawer direction="left">
       <DrawerTrigger asChild>
-        <button aria-label="Toggle Menu">
+        <button>
           <MenuIcon className="h-8 w-8" aria-hidden="true" />
         </button>
       </DrawerTrigger>
       <DrawerContent>
-        <div className="h-full w-full max-w-sm">
+        <div className="h-full w-full gap-5">
           <DrawerHeader>
-            <DrawerTitle>Move Goal</DrawerTitle>
-            <DrawerDescription>Set your daily activity goal.</DrawerDescription>
-          </DrawerHeader>
-          <DrawerFooter>
-            <Button>Submit</Button>
             <DrawerClose asChild>
-              <Button variant="outline">Cancel</Button>
+              <button>
+                <CloseIcon className="h-8 w-8" aria-hidden="true" />
+              </button>
             </DrawerClose>
-          </DrawerFooter>
+          </DrawerHeader>
+          {topics.map((topic) => (
+            <div
+              className="flex justify-between gap-2 border border-b"
+              key={topic.id}
+            >
+              <Link
+                href={`/topic/${topic.id}`}
+                className="p- flex w-full items-center justify-between px-5 py-3 text-slate-300 hover:bg-gray-200 hover:text-background"
+              >
+                <p>{topic.title}</p>
+                <div className="flex items-center gap-2">
+                  <p className=" ">{topic.progress}%</p>
+                  <ArrowRight className="h-6 w-6  " aria-hidden="true" />
+                </div>
+              </Link>
+            </div>
+          ))}
         </div>
       </DrawerContent>
     </Drawer>
