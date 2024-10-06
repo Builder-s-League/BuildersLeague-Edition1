14a5154335a2aad39442dd2bf7758dcafbc6d1c3
Soheil Changizi <cocolico12@gmail.com>
2024-10-06 12:29:07 -0500

    forget-password

--- src/app/hr/forgetPassword/page.tsx
+++ src/app/hr/forgetPassword/page.tsx
@@ -1,9 +1,9 @@
-import { HrForgetPassword } from '@/components/HrForgetPassword'
+import { ForgetPassword } from '@/components/ForgetPassword'
 
 export default function HRFPassPage() {
   return (
     <div className="flex w-full flex-1 flex-col justify-center gap-2 px-8 sm:max-w-md">
-      <HrForgetPassword />
+      <ForgetPassword />
     </div>
   )
 }
