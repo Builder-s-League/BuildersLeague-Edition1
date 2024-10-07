5f52bea7359714fd88fc2a64f9589933c2bbdd0b
Vinicius Okamoto <leo_kamoto@hotmail.com>
2024-10-07 09:37:32 -0500

    Refactor topic page to use mock data for topics and remove unused import

--- src/services/getTopicDetail.ts
+++ src/services/getTopicDetail.ts
@@ -9,7 +9,9 @@ export const getTopicDetail = async ({
   id: string
 }): Promise<Topic> => {
   const response = await fetch(`${API_URL}/api/topic?id=${id}`, {
-    cache: 'no-cache',
+    next: {
+      revalidate: 600,
+    },
   })
   if (!response.ok) {
     throw new Error(`Failed to fetch data: ${response.statusText}`)
