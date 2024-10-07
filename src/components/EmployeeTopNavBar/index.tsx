c992929d08510a74461546e36b3cf8b356f35067
Anmol Sharma <keepupsharma@gmail.com>
2024-10-06 21:49:21 -0500

    fix router

--- src/components/EmployeeTopNavBar/index.tsx
+++ src/components/EmployeeTopNavBar/index.tsx
@@ -3,24 +3,22 @@
 import { ArrowLeftIcon } from 'lucide-react'
 import MenuDrawer from '../MenuDrawer'
 import router from 'next/router'
-import { usePathname } from 'next/navigation'
 import Link from 'next/link'
+import { usePathname, useRouter } from 'next/navigation'
 
 const unallowedRoutes = ['/emp/notes', '/emp/profile-settings']
 
 export function EmployeeTopNavBar() {
   const pathname = usePathname()
   const isUnallowedRoute = unallowedRoutes.includes(pathname)
+  const router = useRouter()
 
   return (
     <nav className="flex w-full flex-row justify-between p-4">
       {!isUnallowedRoute ? (
         <MenuDrawer />
       ) : (
-        <ArrowLeftIcon
-          className="cursor-pointer"
-          onClick={() => router.back()}
-        />
+        <ArrowLeftIcon className="cursor-pointer" onClick={router.back} />
       )}
       <Link href="/feedback">
         <button>Feedback</button>
