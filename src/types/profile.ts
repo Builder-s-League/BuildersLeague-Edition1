4a84eba8d82a766f2f50264ac3d2b1d459ef6dca
Vinicius Okamoto <leo_kamoto@hotmail.com>
2025-02-25 01:43:17 -0600

    feat: update authentication and database utilities

---
+++ src/types/profile.ts
@@ -0,0 +1,38 @@
+/**
+ * @author Vinicius
+ * @created 2025-02-25 1:33p.m.
+ * @description Profile type
+ */
+
+export type Profile = {
+  id: string
+  name: string
+  email: string
+  contact_info: string
+  created_at: string
+  updated_at: string
+  is_active: boolean
+  role: number
+  admin_id: string
+}
+export interface ProfileSummary {
+  id: string
+  name: string
+  email: string
+  contact_info: string
+  is_active: boolean
+}
+
+export interface ProfileUpdate {
+  name: string
+  email: string
+  contact_info: string
+  is_active: boolean
+}
+
+export interface ProfileCreate {
+  name: string
+  email: string
+  contact_info: string
+  password: string
+}
