a9ac44b88461c3e1ae326bcd25f54af38b1388eb
Dai Vo <daidaivo2002@gmail.com>
2025-03-01 03:15:35 -0600

    fix bug after refactoring

--- src/app/emp/(authenticated)/topics/[topicId]/page.tsx
+++ src/app/emp/(authenticated)/topics/[topicId]/page.tsx
@@ -10,19 +10,34 @@ interface TopicDetailPageProps {
 export default async function TopicDetailPage({
   params,
 }: TopicDetailPageProps) {
-  let response = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_API_URL}/topic?id=${{ id: params.topicId }}`,
-    {
-      next: {
-        revalidate: 600,
+  const id = { id: params.topicId }
+  const apiUrl = process.env.CMS_API_URL
+
+  const endpoint = `/topics/${id.id}?locale=undefined&draft=false&depth=1`
+  const apiKey = process.env.CMS_API_KEY
+
+  try {
+    const response = await fetch(apiUrl + endpoint, {
+      headers: {
+        Authorization: `users API-Key ${apiKey}`,
       },
-    },
-  )
-  const responseJson: Topic = await response.json()
+    })
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data: ${response.statusText}`)
+    }
 
-  if (!response.ok) {
-    throw new Error(`Failed to fetch data: ${response.statusText}`)
+    const data = await response.json()
+
+    const responseData = {
+      ...data,
+      progress: 50,
+    }
+
+    console.log(responseData)
+
+    return <TopicDetailPageContent topic={responseData} />
+  } catch (error) {
+    console.error(error)
+    return { error: (error as Error).message }
   }
-
-  return <TopicDetailPageContent topic={responseJson} />
 }
