af8ebe70922d71d09d917f5f3e824c214d05faac
Dai Vo <daidaivo2002@gmail.com>
2025-02-28 23:35:48 -0600

    fix image domain issue

--- next.config.js
+++ next.config.js
@@ -60,6 +60,10 @@ const nextConfig = {
         protocol: 'http',
         hostname: 'localhost',
       },
+      {
+        protocol: 'http',
+        hostname: process.env.CMS_DOMAIN,
+      },
     ],
   },
   experimental: {
